fda254c8a35f60a53f70a7a74d549062ae786d7f
Takon Ajie Njar <44798140+nkimajie@users.noreply.github.com>
2022-02-09 00:05:24 +0100

    push

---
+++ lib/pages/navpages/main_page.dart
@@ -0,0 +1,70 @@
+// ignore_for_file: deprecated_member_use, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:flutter_cubit/pages/navpages/bar_item_page.dart';
+import 'package:flutter_cubit/pages/navpages/home_page.dart';
+import 'package:flutter_cubit/pages/navpages/my_page.dart';
+import 'package:flutter_cubit/pages/navpages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [
+    HomePage(),
+    BarItemPage(),
+    SearchPage(),
+    MyPage()
+  ];
+
+  int currentIndex=0;
+
+  void onTab(int index){
+    setState(() {
+      currentIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        onTap: onTab,
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        unselectedFontSize: 0, // to hide label
+        selectedFontSize: 0,
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(
+            title: Text("Home"),
+            icon: Icon(Icons.apps),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Bar"),
+            icon: Icon(Icons.bar_chart_sharp),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Search"),
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            title: Text("My"),
+            icon: Icon(Icons.person),
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
